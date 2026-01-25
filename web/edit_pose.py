36ede27ddf096de821fa72b1f27639feb8f40dfa
e5c4p3r <jerryhsutw96@gmail.com>
2026-01-25 12:41:18 +0800

    improve pose editing ai

--- web/edit_pose.py
+++ web/edit_pose.py
@@ -10,7 +10,7 @@ def find_conda_env():
     return os.path.join(user_home, "miniconda3", "envs", "ip2p", "python.exe")
 
 
-def run_pose_edit(image_path, prompt, output_path="../instruct-pix2pix/imgs/output.jpg"):
+def run_pose_edit(image_path, advice, output_path="../instruct-pix2pix/imgs/output.jpg"):
     current_dir = os.path.dirname(os.path.abspath(__file__))
 
     target_work_dir = os.path.abspath(os.path.join(current_dir, "../instruct-pix2pix"))
@@ -23,12 +23,21 @@ def run_pose_edit(image_path, prompt, output_path="../instruct-pix2pix/imgs/outp
 
     print(target_work_dir)
 
+    image_guidance = 0.15
+    text_guidance = 10.0
+    steps = 20
+
+    prompt = f"Make a single person stand facing the camera, giving a TED talk presentation, {advice}"
+
     cmd = [
         conda_env,
         script_path,
         "--input", abs_input_path,
         "--output", abs_output_path,
         "--edit", prompt,
+        "--cfg-image", str(image_guidance),
+        "--cfg-text", str(text_guidance),
+        "--steps", str(steps)
     ]
 
     subprocess.run(cmd, cwd=target_work_dir, check=True)
@@ -41,6 +50,5 @@ def run_pose_edit(image_path, prompt, output_path="../instruct-pix2pix/imgs/outp
 
     return ret
 
-
-# if __name__ == "__main__":
-#     result = run_pose_edit("test.jpg", "open the arms wider")
+if __name__ == "__main__":
+    result = run_pose_edit("test.jpg", "open the arms wider")
