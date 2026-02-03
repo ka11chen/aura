c855a1cb6ca50f85a06c876c87766c420fa10a39
e5c4p3r <jerryhsutw96@gmail.com>
2026-02-03 17:31:35 +0800

    fix evaluation

--- web/evaluate.py
+++ web/evaluate.py
@@ -1,12 +1,21 @@
-import numpy as np
 import math
 
-def get_point(landmarks, idx):
-    try:
-        lm = landmarks.pose_landmarks[0].landmark[idx]
-        return np.array([lm.x, lm.y])
-    except:
-        return None
+def get_pts(landmarks):
+    pts = {}
+    for i, lm in enumerate(landmarks):
+        if isinstance(lm, dict):
+            x, y = lm['x'], lm['y']
+            vis = lm.get('visibility', 1.0)
+        else:
+            x, y = lm.x, lm.y
+            vis = getattr(lm, 'visibility', 1.0)
+
+        if vis > 0.3:
+            pts[i] = (x, y)
+    return pts
+
+def calc_dist(lm1, lm2):
+    return math.dist(lm1, lm2)
 
 torso_indices = [11, 12, 23, 24]
 
@@ -14,15 +23,18 @@ def evaluate_pose(original_landmarks, modified_landmarks):
     score = 1.0
     valid_points = 0
 
+    pts_orig = get_pts(original_landmarks.pose_landmarks[0])
+    pts_mod = get_pts(modified_landmarks.pose_landmarks[0])
+
     for idx in torso_indices:
-        p_orig = get_point(original_landmarks, idx)
-        p_mod = get_point(modified_landmarks, idx)
+        p_orig = pts_orig[idx]
+        p_mod = pts_mod[idx]
 
         if p_orig is not None and p_mod is not None:
-            dist = np.linalg.norm(p_orig - p_mod)
+            dist = calc_dist(p_orig, p_mod)
             score -= dist
             valid_points += 1
 
-    score -= 4 - valid_points
+    score -= (4 - valid_points) * 100
 
     return score
